ecbf4de0f402cfa75cd04fce489ae89cf78a7227
kpal <kpal81xd@gmail.com>
2025-09-02 16:43:44 +0100

    feat: add TypeScript type annotations to math and spline modules

--- src/core/math.ts
+++ src/core/math.ts
@@ -1,25 +1,25 @@
 /**
  * Damping function to smooth out transitions.
  *
- * @param {number} damping - Damping factor (0 < damping < 1).
- * @param {number} dt - Delta time in seconds.
- * @returns {number} - Damping factor adjusted for the delta time.
+ * @param damping - Damping factor (0 < damping < 1).
+ * @param dt - Delta time in seconds.
+ * @returns - Damping factor adjusted for the delta time.
  */
-export const damp = (damping, dt) => 1 - Math.pow(damping, dt * 1000);
+export const damp = (damping: number, dt: number) => 1 - Math.pow(damping, dt * 1000);
 
 /**
  * Easing function for smooth transitions.
  *
- * @param {number} x - Input value in the range [0, 1].
- * @returns {number} - Output value in the range [0, 1].
+ * @param x - Input value in the range [0, 1].
+ * @returns - Output value in the range [0, 1].
  */
-export const easeOut = x => (1 - (2 ** (-10 * x))) / (1 - (2 ** -10));
+export const easeOut = (x: number) => (1 - (2 ** (-10 * x))) / (1 - (2 ** -10));
 
 /**
  * Modulus function that handles negative values correctly.
  *
- * @param {number} n - The number to be modulated.
- * @param {number} m - The modulus value.
- * @returns {number} - The result of n mod m, adjusted to be non-negative.
+ * @param n - The number to be modulated.
+ * @param m - The modulus value.
+ * @returns - The result of n mod m, adjusted to be non-negative.
  */
-export const mod = (n, m) => ((n % m) + m) % m;
+export const mod = (n: number, m: number) => ((n % m) + m) % m;
