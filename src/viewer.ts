30bbf000f0955f7d59b36954fe063ee386037392
Donovan Hutchence <slimbuck7@gmail.com>
2025-12-18 14:15:45 +0000

    latest

--- src/viewer.ts
+++ src/viewer.ts
@@ -178,8 +178,9 @@ class Viewer {
 
         // handle HQ mode changes
         const updateHqMode = () => {
-            // keep resolution under 4k on desktop and HD on mobile
-            const maxRatio = (platform.mobile ? 1920 : 3024) / Math.max(screen.width, screen.height);
+            // keep resolution under 4k on desktop and HD on mobile, but use the shorter screen side
+            // so ultra-wide monitors work correctly.
+            const maxRatio = (platform.mobile ? 1080 : 2160) / Math.min(screen.width, screen.height);
 
             // half pixel resolution with hq mode disabled
             graphicsDevice.maxPixelRatio = (state.hqMode ? 1.0 : 0.5) * Math.min(maxRatio, window.devicePixelRatio);
